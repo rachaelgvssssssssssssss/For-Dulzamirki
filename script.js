0c1268ff2146a6eccb80a88b607853f1e0b246ca
rachaelgvssssssssssssss <rachaelgvs@gmail.com>
2026-07-02 20:40:09 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,39 @@
+const terminalOutput = document.getElementById("terminalOutput");
+const startBtn = document.getElementById("startBtn");
 
+const terminalText =
+"$ ./open_my_heart";
+
+let index = 0;
+
+// Tombol disembunyikan dulu
+startBtn.style.display = "none";
+
+// Efek mengetik
+function typeTerminal(){
+
+    if(index < terminalText.length){
+
+        terminalOutput.innerHTML =
+        terminalText.substring(0,index+1) +
+        '<span class="cursor"></span>';
+
+        index++;
+
+        setTimeout(typeTerminal,120);
+
+    }else{
+
+        terminalOutput.innerHTML =
+        terminalText +
+        '<span class="cursor"></span>';
+
+        startBtn.style.display="inline-block";
+
+    }
+
+}
+
+typeTerminal();
+
+startBtn.addEventListener("click",startStory);
